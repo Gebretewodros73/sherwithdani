1cefab140d9ee4ca9401d6f10a88f9f4d8d9b4f5
gebretewodros73 <gebretewodros73@gmail.com>
2023-02-21 04:45:10 +0000

    passes betty

--- shell.c
+++ shell.c
@@ -1,67 +1,51 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 #include "shell.h"
-
 #define MAX_CMD_LEN 1024
-
-
-extern char **environ;
-
-void print_environment();
+/**
+ * main - perform the task
+ * @argc: counts arg
+ * @argv: storse arg
+ * Return: Always 0
+ */
 int main(int argc, char **argv)
 {
-    char *command = NULL;
-    size_t len = 0;
-    char *stkn;
+	char *command = NULL, *stkn;
+	size_t len = 0;
 
-    while (1)
-    {
-        printf("$ ");
-        getline(&command, &len, stdin);
+	while (1)
+	{
+		printf("$ ");
+		getline(&command, &len, stdin);
+		stkn = strtok(command, " \n");
+		char **arr = malloc(sizeof(char *) * (MAX_ARGS + 1));
 
-        stkn = strtok(command, " \n");
+		arr[0] = stkn;
+		int i = 1;
 
-        char **arr = malloc(sizeof(char *) * (MAX_ARGS + 1));
-        arr[0] = stkn;
-
-        int i = 1;
-        while (stkn != NULL)
-        {
-            stkn = strtok(NULL, " \n");
-            arr[i] = stkn;
-            i++;
-        }
-        arr[i] = NULL;
-
-        if (arr[0] == NULL) {
-            free(arr);
-            continue;
-        }
-
-        if (strcmp(arr[0], "exit") == 0) {
-    printf("Exiting shell...\n");
-    exit(0);
-} else if (strcmp(arr[0], "env") == 0) {
-    print_environment();
-} else {
-    execute_run(arr);
-}
-
-        free(arr);
-    }
-
-    free(command);
-    return (0);
-}
-
-void print_environment() {
-    char **env = environ;
-    while (*env) {
-        printf("%s\n", *env);
-        env++;
-    }
+		while (stkn != NULL)
+		{
+			stkn = strtok(NULL, " \n");
+			arr[i] = stkn;
+			i++; }
+		arr[i] = NULL;
+		if (arr[0] == NULL)
+		{
+			free(arr);
+			continue;
+		}
+		if (strcmp(arr[0], "exit") == 0 && (arr[1] == NULL))
+		{
+			printf("Exiting shell...\n");
+			exit(0);
+		}
+		else if (strcmp(arr[0], "env") == 0 && (arr[1] == NULL))
+		{
+			print_environment();
+		}
+		else
+		{
+			execute_run(arr);
+		}
+		free(arr); }
+	free(command);
+	return (0);
 }
--- shell.h
+++ shell.h
@@ -1,10 +1,19 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <errno.h>
+
 #define MAX_ARGS 64
 #define MAX_PATH_LEN 1024
 
-char* get_full_path(char* cmd);
+char *get_full_path(char *cmd);
 void execute_run(char **arr);
+void print_environment(void);
 
-#endif 
+#endif
